ffcd97ab0162e1e4f0d4da0606131b34c7474456
Anton Ivanyutenko <anton.ivanyutenko@gmail.com>
2015-01-27 16:42:49 +0300

    Improve javascript validation, add switch off

--- registration/registration.php
+++ registration/registration.php
@@ -1,3 +1,4 @@
+<?php $javascript_validation = false;?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -20,17 +21,17 @@
                     type="text"
                     name="firstname"
                     placeholder="First name"
-                    pattern="\w+"
+                    <?php echo $javascript_validation ? 'pattern="[A-Za-zА-Яа-яёЁ]+"' : ''; ?>
                     title="First name must consist only from alphabet symbols"
-                    value="<?echo 'Anton'?>"
+                    value="<?php echo 'Anton'?>"
                     required><br><br>
                 <input
                     type="text"
                     name="lastname"
                     placeholder="Last name"
-                    pattern="[\w-]+"
+                    <?php echo $javascript_validation ? 'pattern="[A-Za-zА-Яа-яёЁ-]+"' : ''; ?>
                     title="Last name must consist only from alphabet symbols and hyphen"
-                    value="<?echo 'Ivaniutsenka'?>"
+                    value="<?php echo 'Ivaniutsenka'?>"
                     required><br><br>
                 <input
                     type="text"
@@ -39,24 +40,24 @@
                     name="dateofbirth"
                     placeholder="Date of birth"
                     title="Choose the needed date"
-                    max="<?echo date('Y-m-d')?>"
-                    value="<?echo '1989-03-22'?>"
+                    max="<?php echo date('Y-m-d')?>"
+                    value="<?php echo '1989-03-22'?>"
                     required><br><br>
                 <input type="radio" name="sex" value="male" required checked>Male
                 <input type="radio" name="sex" value="female" required>Female<br><br>
                 <input
-                    type="email"
+                    type="<?php echo $javascript_validation ? 'email' : 'text'; ?>"
                     name="email"
                     placeholder="Email address"
                     title="Email must match for the next pattern: exmple@domain.com"
-                    value="<?echo 'anton.ivanyutenko@gmail.com'?>"
+                    value="<?php echo 'anton.ivanyutenko@gmail.com'?>"
                     required><br><br>
                 <input
                     type="password"
                     name="password"
                     placeholder="Password"
                     onchange="form.password2.pattern = this.value"
-                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+                    <?php echo $javascript_validation ? 'pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"' : ''; ?>
                     title="Password must contain at least 6 characters, including UPPER/lowercase and numbers"
                     value="er34RL1p"
                     required><br><br>
@@ -70,9 +71,9 @@
                 <input
                     type="text"
                     name="phone"
-                    pattern="\+[\d\-]+"
+                    <?php echo $javascript_validation ? 'pattern="\+[\d\-]+"' : ''; ?>
                     title="Phone must match for the next pattern: +375-XX-XXXXXXX"
-                    value="<?echo '+375296957996'?>"
+                    value="<?php echo '+375296957996'?>"
                     placeholder="Mobile phone"><br><br>
                 <input
                     type="file"
